21b7fc1b31bda0f1a6882e7289ab0f91944f40b1
noreldenm7md <41130044+noreldenm7md@users.noreply.github.com>
2018-07-12 00:32:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,9 +6,19 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
+    if (message.content === 'zg') {
     	message.reply('pong');
   	}
+});
+
+client.on("guildMemberAdd", function(member) {
+    const wc = member.guild.channels.find("name", "welcome")
+        const embed = new Discord.RichEmbed()
+        .setColor('00FF01')
+        .setAuthor(member.user.tag, member.user.avatarURL)
+        .setFooter("Welcome To Codes Shop.")
+        .setTimestamp()
+        return wc.sendEmbed(embed);
 });
 
 // THIS  MUST  BE  THIS  WAY
