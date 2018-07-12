85e82a4b2effc0fc33ce21ee8f06ce94f47cc467
noreldenm7md <41130044+noreldenm7md@users.noreply.github.com>
2018-07-12 07:17:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,60 +21,47 @@ client.on("guildMemberAdd", function(member) {
 });
 
 
-  client.on('message', async rokz => {
-    if(rokz.content.startsWith(prefix + "تقديم")) {
-      let lang = '';
-      let time = '';
-      let expe = '';
-      let fillter = m => m.author.id === rokz.author.id
-      await rokz.channel.send("ما اسمك ؟").then(e => {
-     rokz.channel.awaitMessages(fillter, { time: 60000, max: 1 })
-     .then(co => {
-       lang = co.first().content;
-        co.first().delete();
-
-       e.edit(`هل سبق ان عملت في متجر من قبل
-[${lang}]`)
-       rokz.channel.awaitMessages(fillter, { time: 60000, max: 1 })
-       .then(col => {
-         time = col.first().content;
-          col.first().delete();
-
-            e.edit(`ماذا سوف تبيع ؟
-[${time}]
-[${lang}]`)
-            rokz.channel.awaitMessages(fillter, { time: 60000, max: 1 })
-            .then(coll => {
-              expe = coll.first().content;
-               coll.first().delete();
-
-               e.edit(`جاري تقديمك...
-[${expe}]
-[${time}]
-[${lang}]`)
-              let rokzz = rokz.guild.channels.find("name", "التقديمات")
-              setTimeout(() => {
-                e.edit("تم التقديم")
-              }, 3000)
-              rokz.send(`
-                تقديم من ${rokz.author}
-
-  الاسم :
-  \`${lang}\`
- هل سبق ان عملت في متجر من قبل :
-  \`${time}\`
- ماذا سوف تبيع :
-  \`${expe}\`
-                `).then(rokzzz => {
-                  rokzzz.react("✅")
-                  rokzzz.react("❌")
-                })
-            })
-       })
-     })
-   })
-    }
-  })
+client.on('message', async message => {
+  if(message.content.startsWith(prefix + "تقديم")) {
+    await message.channel.send("`ما اسمك`").then(e => {
+    let filter = m => m.author.id === message.author.id
+    let lan = '';
+    let md = '';
+    let br = '';
+    let chaLan = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
+    .then(collected => {
+      lan = collected.first().content
+      collected.first().delete()
+e.edit(`\`هل سبق وان كنت في متجر [${lan}]\``)
+let chaMd = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
+.then(co => {
+  md = co.first().content
+        co.first().delete()
+        e.edit(`\`ماذا ستبيع[${lan}]\``)
+let br = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
+.then(col => {
+  br = col.first().content
+        col.first().delete()
+e.edit("`جاري التقديم علي طلبك...`").then(b => {
+        setTimeout(() => {
+  b.edit(`**تم التقديم وسيتم الرد فـ اقرب وقت**`)
+        },2000);
+var gg = message.guild.channels.find('name', 'التقديمات')
+if(!gg) return;
+if(gg) {
+gg.send({embed : new Discord.RichEmbed()
+.setTitle('**تقديم رتبة سبورت**')
+.setDescription(`الاسم : /n${lan}\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- n\هل سبق وان كنت في متجر : ${md} \n =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\nماذا ستبيع : \n${br} =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- \nتم التقديم علي رتبة بائع بواسطة : \n${message.author.id}`)  
+.setTimestamp()
+});
+}        
+})
+})
+})
+})
+})
+ }
+})
 
 
 // THIS  MUST  BE  THIS  WAY
